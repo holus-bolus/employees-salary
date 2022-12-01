acd60b80f5b2e658adb4f4be74cc8ec6b417241a
holus-bolus <ustinovoleksij@gmail.com>
2022-12-01 21:05:54 +0200

    Added employees and filter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import './styles/index.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app/app';
+
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
