6c59e87d81440458abd03d849c9b13e5b1ca4266
holus-bolus <ustinovoleksij@gmail.com>
2022-12-02 13:40:33 +0200

    Added ф сдфіі ьуa class method render

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,17 +8,20 @@ import "./app.css";
 
 function App() {
   const data = [
-    { name: "Sheldon Cooper", salary: 8000, increase: false },
-    { name: "Gregory House", salary: 3000, increase: true },
-    { name: "Wednesday Addams", salary: 5000, increase: false },
+    { name: "John C.", salary: 800, increase: false, id: 1 },
+    { name: "Alex M.", salary: 3000, increase: true, id: 2 },
+    { name: "Carl W.", salary: 5000, increase: false, id: 3 },
   ];
+
   return (
     <div className="app">
       <AppInfo />
+
       <div className="search-panel">
         <SearchPanel />
         <AppFilter />
       </div>
+
       <EmployeesList data={data} />
       <EmployeesAddForm />
     </div>
