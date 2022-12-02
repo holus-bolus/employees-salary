ebac4522d9d2d047a2cf4be9300ffbe41cb1e3d7
holus-bolus <ustinovoleksij@gmail.com>
2022-12-02 14:30:26 +0200

    Added ф a filter method

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from "react";
+
 import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
 import AppFilter from "../app-filter/app-filter";
@@ -5,27 +7,47 @@ import EmployeesList from "../employees-list/employees-list";
 import EmployeesAddForm from "../employees-add-form/employees-add-form";
 
 import "./app.css";
+import { render } from "react-dom";
 
-function App() {
-  const data = [
-    { name: "John C.", salary: 800, increase: false, id: 1 },
-    { name: "Alex M.", salary: 3000, increase: true, id: 2 },
-    { name: "Carl W.", salary: 5000, increase: false, id: 3 },
-  ];
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { name: "John C.", salary: 800, increase: false, id: 1 },
+        { name: "Alex M.", salary: 3000, increase: true, id: 2 },
+        { name: "Carl W.", salary: 5000, increase: false, id: 3 },
+      ],
+    };
+  }
 
-  return (
-    <div className="app">
-      <AppInfo />
+  deleteItem = (id) => {
+    this.setState(({ data }) => {
+      // const index = data.findIndex((elem) => elem.id === id);
+      // const before = data.slice(0, index);
+      // const after = data.slice(index + 1);
+      //
+      // const newArr = [...before, ...after];
+      return {
+        data: data.filter((item) => item.id !== id),
+      };
+    });
+  };
+  render() {
+    return (
+      <div className="app">
+        <AppInfo />
 
-      <div className="search-panel">
-        <SearchPanel />
-        <AppFilter />
+        <div className="search-panel">
+          <SearchPanel />
+          <AppFilter />
+        </div>
+
+        <EmployeesList data={this.state.data} onDelete={this.deleteItem} />
+        <EmployeesAddForm />
       </div>
-
-      <EmployeesList data={data} />
-      <EmployeesAddForm />
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
